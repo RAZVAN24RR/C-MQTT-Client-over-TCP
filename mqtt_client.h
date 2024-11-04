dc732084f5cc2405b834f8bd020719cb47da6e79
RAZVAN24RR <razvanandreipasaran@gmail.com>
2024-11-04 13:38:45 +0200

    lib .h files

---
+++ mqtt_client.h
@@ -0,0 +1,21 @@
+#ifndef MQTT_CLIENT_H
+#define MQTT_CLIENT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define MQTT_PORT 1883
+#define MQTT_KEEPALIVE 60
+
+int mqtt_connect(int socktfd, const char *client_id);
+int mqtt_publish(int socktfd, const char *topic, const char *message);
+int mqtt_subscribe(int socktfd, const char *topic);
+int mqtt_ping(int socktfd);
+void mqtt_disconnect(int socktfd);
+
+#endif // ! MQTT_CLIENT_H
